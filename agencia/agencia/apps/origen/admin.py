b2d72ca02d67a033281cf53c1bbf24e7bceabd5f
Jose Alvarez Arrieta <83204155+JoseAlvarezArrieta@users.noreply.github.com>
2021-09-30 00:21:08 -0500

    modelo origen + modelo referencias_familiares

---
+++ agencia/agencia/apps/origen/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from apps.origen.models import Origen
+
+# Register your models here.
+
+admin.site.register(Origen)
---
+++ agencia/agencia/apps/referencias_familiares/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from apps.referencias_familiares.models import ReferenciaFamiliar
+
+# Register your models here.
+
+admin.site.register(ReferenciaFamiliar)
