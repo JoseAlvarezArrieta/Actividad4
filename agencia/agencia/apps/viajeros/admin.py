fd45f6e3056fcb0be8c6c6d3a9d976e4b0f98ffa
YoserSerna <90435054+YoserSerna@users.noreply.github.com>
2021-09-30 00:22:51 -0500

    creación de los modelos viajeros y viajes.

---
+++ agencia/agencia/apps/viajeros/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from apps.viajeros.models import Viajero
+
+# Register your models here.
+
+admin.site.register(Viajero)
---
+++ agencia/agencia/apps/viajes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from apps.viajes.models import Viaje
+
+# Register your models here.
+
+admin.site.register(Viaje)
